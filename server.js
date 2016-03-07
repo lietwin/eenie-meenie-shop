f0709b17e56845b25fa28dca4ecae08105da4094
lietwin <quicklics@gmail.com>
2016-03-07 22:59:53 +0100

    add express for node server

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+module.exports = () => {
+  const app = express();
+
+  app.get("/", function(req, res){
+    res.send("Hello, Meanote!");
+  });
+
+  app.get("/user/:user", function(req, res){
+    res.send("Page for user "+ req.params.user + " with option " + req.query.option);
+  });
+  
+return app;
+};
