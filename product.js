b18725218b9c27c658483736737d8247c2cb1b2e
lietwin <quicklics@gmail.com>
2016-03-12 20:08:47 +0100

    updating product schema with virtual properties and custom setters

--- product.js
+++ product.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const fx = require('./fx');
 
 const productSchema = {
   name: { type: String, required: true },
@@ -7,12 +8,43 @@ const productSchema = {
   pictures: [{ type: String, match: /^http:\/\//i }],
   price: {
     amount: { type: Number, required: true },
-
+    set: (value) => {
+      this.internal.approximatePriceUSD =
+        value / (fx()[this.price.currency] || 1);
+      return value;
+    }
     // Only 3 currencies for now
-    currency: { type: String, enum: ['USD', 'EUR', 'GBP'], required: true }
+    currency: { type: String, enum: ['USD', 'EUR', 'GBP'], required: true },
+    set: (value) => {
+      this.internal.approximatePriceUSD =
+        this.price.amount / (fx()[value] || 1);
+      return value;
+    }
   },
-  category: require('./category').categorySchema
+  category: require('./category').categorySchema,
+  internal: {
+    approximatePriceUSD: Number
+  }
 };
+
+const currencySymbols = {
+  'USD': '$',
+  'EUR': '€',
+  'GBP': '£'
+};
+
+/*
+ * Human-readable string form of price - "$25" rather
+ * than "25 USD"
+ */
+schema.virtual('displayPrice').get(function() {
+  return currencySymbols[this.price.currency] +
+    '' + this.price.amount;
+});
+// For Mongoose to convert js <-> json
+
+schema.set('toObject', { virtuals: true });
+schema.set('toJSON', { virtuals: true });
 
 module.exports = new mongoose.Schema(productSchema);
 module.exports.productSchema = productSchema;
